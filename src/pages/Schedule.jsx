b7eca49f38f26ef29daccbb7a6b32319c0621c9c
Sergi Tsivtsivadze <tserj1993@icloud.com>
2026-05-13 13:14:04 +0400

    Sync calendar language with site language switcher

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -2,7 +2,7 @@ import { useLanguage } from '../context/LanguageContext'
 import './InnerPage.css'
 
 export default function Schedule() {
-  const { t } = useLanguage()
+  const { t, lang } = useLanguage()
   return (
     <>
       <section className="page-hero">
@@ -24,7 +24,7 @@ export default function Schedule() {
             {/* Desktop: Week View */}
             <iframe 
               className="calendar-iframe desktop-only"
-              src="https://calendar.google.com/calendar/embed?src=stdancegroup%40gmail.com&ctz=Asia%2FTbilisi&mode=WEEK&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&hl=ka" 
+              src={`https://calendar.google.com/calendar/embed?src=stdancegroup%40gmail.com&ctz=Asia%2FTbilisi&mode=WEEK&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&hl=${lang}`} 
               style={{ 
                 position: 'absolute', 
                 top: 0, 
@@ -41,7 +41,7 @@ export default function Schedule() {
             {/* Mobile: Agenda View (hides empty time slots) */}
             <iframe 
               className="calendar-iframe mobile-only"
-              src="https://calendar.google.com/calendar/embed?src=stdancegroup%40gmail.com&ctz=Asia%2FTbilisi&mode=AGENDA&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&hl=ka" 
+              src={`https://calendar.google.com/calendar/embed?src=stdancegroup%40gmail.com&ctz=Asia%2FTbilisi&mode=AGENDA&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&hl=${lang}`} 
               style={{ 
                 position: 'absolute', 
                 top: 0, 
