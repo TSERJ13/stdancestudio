5a2faeb4dfc637454e67d9f28997d43bf05959c9
Sergi Tsivtsivadze <tserj1993@icloud.com>
2026-05-13 13:05:19 +0400

    Fix: Absolute notch coverage and Agenda view for mobile calendar to hide empty hours

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -21,7 +21,9 @@ export default function Schedule() {
       <section className="section">
         <div className="container">
           <div className="calendar-wrap">
+            {/* Desktop: Week View */}
             <iframe 
+              className="calendar-iframe desktop-only"
               src="https://calendar.google.com/calendar/embed?src=stdancegroup%40gmail.com&ctz=Asia%2FTbilisi&mode=WEEK&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&hl=ka" 
               style={{ 
                 position: 'absolute', 
@@ -33,7 +35,24 @@ export default function Schedule() {
               }} 
               frameBorder="0" 
               scrolling="no"
-              title="Schedule"
+              title="Schedule Desktop"
+            ></iframe>
+
+            {/* Mobile: Agenda View (hides empty time slots) */}
+            <iframe 
+              className="calendar-iframe mobile-only"
+              src="https://calendar.google.com/calendar/embed?src=stdancegroup%40gmail.com&ctz=Asia%2FTbilisi&mode=AGENDA&showTitle=0&showNav=1&showDate=1&showPrint=0&showTabs=0&showCalendars=0&showTz=0&hl=ka" 
+              style={{ 
+                position: 'absolute', 
+                top: 0, 
+                left: 0, 
+                width: '100%', 
+                height: '100%', 
+                border: 0 
+              }} 
+              frameBorder="0" 
+              scrolling="no"
+              title="Schedule Mobile"
             ></iframe>
           </div>
         </div>
